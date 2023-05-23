804ec36e1e529c3a3d5e6ea00f9687509649401c
aosiang <a8102158@gmail.com>
2023-05-24 00:11:19 +0800

    initial commit

---
+++ src/main/java/idv/tha101/extractp/ExtractpApplication.java
@@ -0,0 +1,13 @@
+package idv.tha101.extractp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idv/tha101/extractp/ExtractpApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.tha101.extractp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtractpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
