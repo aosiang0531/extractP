62fa5f22678a56bb64e57b132c22c4c68da85ec6
MengPing_孟頻 <mengping0110@gmail.com>
2023-05-29 14:58:03 +0800

    add feature/member

---
+++ src/main/java/idv/tha101/extractp/member/service/MemberService.java
@@ -0,0 +1,10 @@
+package idv.tha101.extractp.member.service;
+
+import idv.tha101.extractp.base.service.BaseService;
+import idv.tha101.extractp.member.pojo.MemberVO;
+
+public interface MemberService extends BaseService<MemberVO>{
+
+//	List<MemberVO> findByOrder_status(String status);
+
+}
