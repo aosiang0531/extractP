f2f04c6c2fe77582487452600eb8df6019cc3839
Lucy <iucyyoua@gmail.com>
2023-05-29 15:45:22 +0800

    add feature/class

---
+++ src/main/java/idv/tha101/extractp/clazz/service/MemberClazzSignedService.java
@@ -0,0 +1,8 @@
+package idv.tha101.extractp.clazz.service;
+
+import idv.tha101.extractp.base.service.BaseService;
+import idv.tha101.extractp.clazz.pojo.MemberClazzSignedVO;
+
+public interface MemberClazzSignedService extends BaseService<MemberClazzSignedVO> {
+
+}
---
+++ src/main/java/idv/tha101/extractp/clazz/service/impl/ClazzServiceImpl.java
@@ -0,0 +1,72 @@
+package idv.tha101.extractp.clazz.service.impl;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import idv.tha101.extractp.clazz.dao.ClazzRepository;
+import idv.tha101.extractp.clazz.pojo.ClazzVO;
+import idv.tha101.extractp.clazz.service.ClazzService;
+
+
+@Service
+public class ClazzServiceImpl implements ClazzService {
+	@Autowired
+	private ClazzRepository clazzRepositry;
+
+	@Override
+	public List<ClazzVO> findAll() {
+		return clazzRepositry.findAll();
+	}
+
+	@Override
+	public ClazzVO findById(Integer id) {
+		return clazzRepositry.findById(id).orElseThrow();
+	}
+
+	@Override
+	public ClazzVO saveOrUpdate(ClazzVO vo) {
+		if (vo.getId() != null) {
+			Optional<ClazzVO> optionalVO = clazzRepositry.findById(vo.getId());
+			if (optionalVO.isPresent()) {
+				ClazzVO existingVO = optionalVO.get();
+
+				Class<?> voClass = ClazzVO.class;
+				Field[] fields = voClass.getDeclaredFields();
+
+				for (Field field : fields) {
+					field.setAccessible(true);
+					try {
+						Object updatedValue = field.get(vo);
+						if (updatedValue != null) {
+							field.set(existingVO, updatedValue);
+						}
+					} catch (IllegalAccessException e) {
+						e.printStackTrace();
+					}
+				}
+
+				return clazzRepositry.save(existingVO);
+			} else {
+				return null;
+			}
+		} else {
+			return clazzRepositry.save(vo);
+		}
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		clazzRepositry.deleteById(id);
+	}
+
+//	@Override
+//	public List<ClazzVO> findByClazzStatus(String classStatus) {
+//		// TODO Auto-generated method stub
+//		return clazzRepositry.findByClazzStatus(classStatus);
+//	}
+
+	}
