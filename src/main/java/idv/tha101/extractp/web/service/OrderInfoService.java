35416e9b24a912c5fdac1c6f3ca15670d7fae000
yu <wu5887474@gmail.com>
2023-06-02 18:04:27 +0800

    feature/order hotfix

--- src/main/java/idv/tha101/extractp/web/service/OrderInfoService.java
+++ src/main/java/idv/tha101/extractp/web/service/OrderInfoService.java
@@ -1,36 +1,19 @@
 package idv.tha101.extractp.web.service;
 
-import java.util.Collection;
 import java.util.List;
 
 import idv.tha101.extractp.base.service.BaseService;
-import idv.tha101.extractp.web.pojo.OrderDTO;
 import idv.tha101.extractp.web.pojo.OrderInfoVO;
 
 public interface OrderInfoService extends BaseService<OrderInfoVO>{
 	
 	List<OrderInfoVO> findPageByOrderStatus(Integer member_id,String status);
 	
-	Collection<OrderDTO> findOrderInfo(Integer id);
-
-	List<OrderInfoVO> findAll();
-
-	OrderInfoVO findById(Integer id);
-
-	void deleteById(Integer id);
-
-	OrderInfoVO saveOrUpdate(OrderInfoVO vo);
-
 	List<OrderInfoVO> findByMemberId(Integer member_id);
 
 	List<OrderInfoVO> findPageByPaymentStatus(Integer member_id, String status);
 
 	List<OrderInfoVO> findPageByShippingStatus(Integer member_id, String status);
 
-	void deleteByOrderDetailId(Integer id);
-
-	List<Double> countSubTotal(Integer id);
-
-	double countTotal(Integer id);
 
 }
