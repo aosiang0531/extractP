58665405407caa4e21bae1236362fbbf7294fdc6
kent <kent1648@gmail.com>
2023-06-04 02:11:13 +0800

    add map

--- src/main/java/idv/tha101/extractp/store/service/StoreService.java
+++ src/main/java/idv/tha101/extractp/store/service/StoreService.java
@@ -4,12 +4,16 @@ import idv.tha101.extractp.store.pojo.Store;
 
 import java.util.List;
 
-public interface StoreService<T> {
-    List<T> findAll();
+public interface StoreService {
+    List<Store> findAll();
 
     Store findById(Integer id);
 
     void deleteById(Integer id);
 
     Store save(Store store);
+
+    Store update(int id, Store updateStore);
+
+    List<String> findAllAddresses();
 }
--- src/main/java/idv/tha101/extractp/store/service/impl/StoreServiceImpl.java
+++ src/main/java/idv/tha101/extractp/store/service/impl/StoreServiceImpl.java
@@ -3,13 +3,17 @@ package idv.tha101.extractp.store.service.impl;
 import idv.tha101.extractp.store.dao.StoreRepository;
 import idv.tha101.extractp.store.pojo.Store;
 import idv.tha101.extractp.store.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StoreServiceImpl implements StoreService {
 
+    @Autowired
     private StoreRepository repository;
 
     @Override
@@ -30,5 +34,59 @@ public class StoreServiceImpl implements StoreService {
     @Override
     public Store save(Store store) {
         return repository.save(store);
+    }
+
+    @Override
+    public Store update(int id, Store updateStore) {
+        Optional<Store> optionalStore = repository.findById(id);
+        if (optionalStore.isPresent()) {
+            Store store = optionalStore.get();
+
+            if (updateStore.getStoreTax() != null) {
+                store.setStoreTax(updateStore.getStoreTax());
+            }
+
+            if (updateStore.getMemberId() != null) {
+                store.setMemberId(updateStore.getMemberId());
+            }
+
+            if (updateStore.getStoreName() != null) {
+                store.setStoreName(updateStore.getStoreName());
+            }
+
+            if (updateStore.getStoreInfo() != null) {
+                store.setStoreInfo(updateStore.getStoreInfo());
+            }
+
+            if (updateStore.getStoreAddress() != null) {
+                store.setStoreAddress(updateStore.getStoreAddress());
+            }
+
+            if (updateStore.getStoreTime() != null) {
+                store.setStoreTime(updateStore.getStoreTime());
+            }
+
+            if (updateStore.getStorePhone() != null) {
+                store.setStorePhone(updateStore.getStorePhone());
+            }
+
+            if (updateStore.getStoreWebsite() != null) {
+                store.setStoreWebsite(updateStore.getStoreWebsite());
+            }
+            return repository.save(store);
+        }
+        return null;
+    }
+
+    @Override
+    public List<String> findAllAddresses() {
+        List<Store> stores = repository.findAll();
+        List<String> addresses = new ArrayList<>();
+
+        for (Store store : stores) {
+            addresses.add(store.getStoreAddress());
+        }
+
+        return addresses;
     }
 }
