35431708f72129e141d96cea9312aa441d182a2e
aosiang <a8102158@gmail.com>
2023-06-13 03:05:29 +0800

    feature/chat version 1.0

--- src/main/java/idv/tha101/extractp/ExtractpApplication.java
+++ src/main/java/idv/tha101/extractp/ExtractpApplication.java
@@ -2,10 +2,8 @@ package idv.tha101.extractp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ExtractpApplication {
 
 	public static void main(String[] args) {
--- src/main/java/idv/tha101/extractp/base/config/AuditorAwareConfig.java
+++ src/main/java/idv/tha101/extractp/base/config/AuditorAwareConfig.java
@@ -1,15 +1,31 @@
 package idv.tha101.extractp.base.config;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Component("auditorAware")
-public class AuditorAwareConfig implements AuditorAware<String> {
-	@Override
-	public Optional<String> getCurrentAuditor() {
-		return Optional.of("1");
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "getCreator", dateTimeProviderRef = "getTime")
+public class AuditorAwareConfig {
+	@Bean
+	public AuditorAware<String> getCreator() {
+		return new AuditorAware<String>() {
+			@Override
+			public Optional<String> getCurrentAuditor() {
+				return Optional.of("1");
+			}
+
+		};
 	}
 
+	@Bean
+	public DateTimeProvider getTime() {
+		System.out.println(LocalDateTime.now());
+		return () -> Optional.of(LocalDateTime.now());
+	}
 }
